a1026ae5b3fa20b8c00f2abdb2cc67786fa9b5d4
Pedro Landolt <pedrolandolt88@gmail.com>
2023-05-09 00:45:02 +0100

    TCW add front_end to login page

--- pages/login.php
+++ pages/login.php
@@ -12,7 +12,45 @@
 
   $db = getDatabaseConnection();
 
-  drawHeader($session);
-  drawLoginForm();
+  drawHeader($session); 
+?>
+
+<section class="login">
+  <div class="logo">
+    <img src="../sources/PourProblems_Logo_Gray.png" alt="logo">
+  </div>
+
+
+  <div class="login-input">
+    <h2 class="title">Login</h2>
+    <form action="../actions/action_login.php" method="post" class="login-container">
+      
+    <!-- Email and password -->
+      <input type="email" name="email" placeholder="email">
+      <input type="password" name="password" placeholder="password">
+      
+      <div class="login-options">
+        <!-- Login button -->
+        <button type="submit"><span class="login-btn"><i class="fas fa-arrow-right"></i></span></button>
+        
+        <!-- Stay logged in -->
+        <div class="stay-logged-in">
+          <input type="checkbox" id="stay_logged_in_check" value="true">
+          <label for="stay_logged_in_check">Stay logged in</label>
+        </div>
+      </div>
+
+      <!-- Links -->
+      <div class="login-links">
+        <a class="forgot-password" href="/pages/forgot_password.php">Forgot your password?</a>
+        <a class="register" href="/pages/signup.php">Don't have an account?</a>
+      </div>
+    </form>
+
+  </div>
+</section>
+
+
+<?php
   drawFooter();
 ?>
