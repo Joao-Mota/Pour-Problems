edb753c21e0d9a05b74e26193bd5c01de0354388
Jcoelho13 <up202004846@up.pt>
2023-05-19 16:38:25 +0100

    TCW change users.php

--- pages/users.php
+++ pages/users.php
@@ -62,61 +62,72 @@ drawHeader($session);
             </form>
           </div>
         </div>
-      <?php } else if ($user->role_id == 2) { ?>
-          <div class="agent">
-            <div class="question">
-              <h3>
-              <?= $user->username ?>
-              </h3>
+      <?php } 
+      else if($user->role_id == 2) { ?>
+            <div class="agent">
+                <div class="question">
+                <h3> <?= $user->username ?> </h3>
+                </div>
+
+                <div class="answer">
+                <p> Name : <?= $user->fullname ?> </p>
+                </div>
+
+                <div>
+                  <p>This Agent belongs to the following departments:</p>
+                  <ul>
+                    <?php $user_department = User_Department::getDepartmentsFromUser($db, $user->id);
+                       foreach($departments as $department) {
+                        foreach($user_department as $user_dep){
+                        if ($department->id == $user_dep->department_id) {
+                          ?>
+                          <li> <?=$department->name?>
+                          <form action="../actions/action_delete_department_from_user.php" method="post" class="delete">
+                            <input type="hidden" name="user_id" value="<?=$user->id?>">
+                            <input type="hidden" name="department_id" value="<?=$department->id?>">
+                            <input type="submit" value="Delete">
+                          </form>
+                        </li>
+                        <?php }}} ?>
+                  </ul>
+                </div>
+
+                <div>
+                  <?php $departments_not_assigned = User_Department::getDepartmentsNotFromUser($db, $user->id);
+                  if (!empty($departments_not_assigned)) { ?>
+                  <form action="../actions/action_assign_department.php" method="post" class="delete">
+
+                    <input type="hidden" name="user_id" value="<?=$user->id?>">
+
+                    <select id="departments" name="department">
+
+                      <?php 
+                          foreach($departments_not_assigned as $department) { ?>
+                          <option value="<?=$department->name?>"><?=$department->name?></option>
+                          <?php } ?>
+                    </select>
+
+                    <input type="submit" value="Assign Department"> 
+                    
+
+                  </form>
+                  <?php } ?>
+                </div>
+
+                <div>
+                  <form action="../actions/action_change_to_client.php" method="post" class="delete">
+
+                    <input type="hidden" name="user_id" value="<?=$user->id?>">
+
+                    
+
+                    <input type="submit" value="Turn Back to client"> 
+
+                  </form>
+                </div>
             </div>
-
-            <div class="answer">
-              <p> Name :
-              <?= $user->fullname ?>
-              </p>
-            </div>
-
-            <div>
-              <form action="../actions/action_assign_department.php" method="post" class="delete">
-
-                <input type="hidden" name="user_id" value="<?= $user->id ?>">
-
-                <select id="departments" name="department">
-
-                <?php foreach ($departments as $department) { ?>
-                    <option value="<?= $department->name ?>"> <?= $department->name ?> </option>
-                <?php } ?>
-
-                </select>
-
-                <input type="submit" value="Assign Department">
-
-              </form>
-            </div>
-
-            <div>
-              <form action="../actions/action_change_role.php" method="post" class="delete">
-
-                <input type="hidden" name="user_id" value="<?= $user->id ?>">
-                <input type="hidden" name="role_id" value="1">
-
-                <input type="submit" value="Make Admin">
-
-              </form>
-            </div>
-
-            <div>
-              <form action="../actions/action_change_role.php" method="post" class="delete">
-
-                <input type="hidden" name="user_id" value="<?= $user->id ?>">
-                <input type="hidden" name="role_id" value="3">
-
-                <input type="submit" value="Make Client">
-
-              </form>
-            </div>
-          </div>
-      <?php } else { ?>
+        <?php }
+       else { ?>
           <div class="user">
             <div class="question">
               <h3>
