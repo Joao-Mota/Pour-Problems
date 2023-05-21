4714b6636be3874f2cabbc2bc7188d604c896446
Pedro Landolt <pedrolandolt88@gmail.com>
2023-05-21 23:49:27 +0100

    TCW final toutchs

--- database/message.class.php
+++ database/message.class.php
@@ -8,16 +8,14 @@ class Message
     public string $datetime;
     public int $user_id;
     public int $ticket_id;
-    public array $files;
 
-    public function __construct(int $id, string $text, string $datetime, int $user_id, int $ticket_id, array $files = array())
+    public function __construct(int $id, string $text, string $datetime, int $user_id, int $ticket_id)
     {
         $this->id = $id;
         $this->text = $text;
         $this->datetime = $datetime;
         $this->user_id = $user_id;
         $this->ticket_id = $ticket_id;
-        $this->files = $files;
     }
 
     static function getMessage(PDO $db, int $id): Message
@@ -27,12 +25,7 @@ class Message
         $stmt->execute(array($id));
         $message = $stmt->fetch();
 
-        // get all files from Message_Files table for this message
-        $stmt = $db->prepare('SELECT * FROM Message_Files WHERE message_id = ?');
-        $stmt->execute(array($id));
-        $files = $stmt->fetchAll();
-
-        return new Message($message['id'], $message['text'], $message['datetime'], $message['user_id'], $message['ticket_id'], $files);
+        return new Message($message['id'], $message['text'], $message['datetime'], $message['user_id'], $message['ticket_id']);
     }
 
     static function getMessages_from_ticket(PDO $db, int $ticket_id): array
@@ -44,18 +37,12 @@ class Message
 
         while ($message = $stmt->fetch()) {
 
-            // get all files from Message_Files table for this message
-            $stmt = $db->prepare('SELECT * FROM Message_Files WHERE message_id = ?');
-            $stmt->execute(array($message['id']));
-            $files = $stmt->fetchAll();
-
             $messages[] = new Message(
                 $message['id'],
                 strval($message['text']),
                 strval($message['datetime']),
                 $message['user_id'],
-                $message['ticket_id'],
-                $files
+                $message['ticket_id']
             );
         }
         return $messages;
